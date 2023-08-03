8fa5541dd0802438aee162e4f577e5b03123451d
Shaan Khan <shaankhan101@gmail.com>
2023-08-03 22:40:03 +1000

    Time: 55 ms (95.65%), Space: 43.4 MB (73.98%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
@@ -4,10 +4,23 @@ function twoSum(numbers: number[], target: number): number[] {
     // Return the indices of the two numbers, added by 1 as an integer array
     // Exactly 1 solution don't use twice
     
-    for(let i = 0; i < numbers.length; i++) {
-        for(let j = 0; j < numbers.length; j++) {
-            if(i != j && numbers[i] + numbers[j] == target)
-                return [i + 1, j + 1];
-        }
+    // for(let i = 0; i < numbers.length; i++) {
+    //     for(let j = 0; j < numbers.length; j++) {
+    //         if(i != j && numbers[i] + numbers[j] == target)
+    //             return [i + 1, j + 1];
+    //     }
+    // }
+    
+    let left: number = 0;
+    let right: number = numbers.length - 1;
+    
+    while(left <= right) {
+        let sum: number = numbers[left] + numbers[right];
+        if(sum === target) return [left + 1, right + 1];
+        else if (sum <= target) left++;
+        else right--;
     }
+    
+    
+    
 };
