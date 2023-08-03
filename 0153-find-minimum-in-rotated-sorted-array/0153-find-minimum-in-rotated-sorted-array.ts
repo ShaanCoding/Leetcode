f1fd400ca2b1c3fd95a5f827e42d08b10bc5b453
Shaan Khan <shaankhan101@gmail.com>
2023-08-03 11:22:09 +1000

    Time: 57 ms (84.09%), Space: 43.2 MB (90.91%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.ts
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.ts
@@ -1,12 +1,32 @@
 function findMin(nums: number[]): number {
     // N is sorted in ascending order, is rotated between 1 and n times
-
     // Rotating an array return minimum number O(logn) -> binary search
 
-    let minimum = Number.MAX_SAFE_INTEGER;
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] < minimum) minimum = nums[i];
+//     Normal binary sort is
+//     Need to find a number where left > right... since sorted right is lowest value
+    
+//     If length is 1
+    if(nums.length === 1) return nums[0];
+    
+    
+    let low: number = 0;
+    let high: number = nums.length - 1;
+    
+    if(nums[high] > nums[low]) return nums[0];
+    
+    while(high >= low) {
+        let midIndex = Math.floor(low + (high - low) / 2);
+        
+        if(nums[midIndex] > nums[midIndex + 1]) return nums[midIndex + 1];
+        if(nums[midIndex - 1] > nums[midIndex]) return nums[midIndex];
+        
+        if(nums[midIndex] > nums[0]) {
+            low = midIndex + 1;
+        } else {
+            high = midIndex - 1;
+        }
+        
     }
-
-    return minimum;
+    
+    return -1;
 };
