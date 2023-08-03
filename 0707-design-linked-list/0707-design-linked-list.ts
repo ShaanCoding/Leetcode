42da802b74a8cb9a43fdb4824afa1606fe85bb78
Shaan Khan <shaankhan101@gmail.com>
2023-08-03 21:17:38 +1000

    Time: 119 ms (91.91%), Space: 51.7 MB (17.34%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.ts
+++ 0707-design-linked-list/0707-design-linked-list.ts
@@ -1,92 +1,92 @@
 class LLNode {
-    val: number;
-    next: LLNode | null;
+  val: number;
+  next: LLNode | null;
 
-    constructor(val?: number, next?: LLNode | null) {
-        this.val = val ? val : 0;
-        this.next = next ? next : null;
-    }
+  constructor(val?: number, next?: LLNode | null) {
+    this.val = val ? val : 0;
+    this.next = next ? next : null;
+  }
 }
 
 class MyLinkedList {
-    head: LLNode;
-    size: number;
+  head: LLNode;
+  size: number;
 
-    constructor() {
-        this.head = null;
-        this.size = 0;
-    }
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
 
-    getNode(index: number): LLNode {
-  if (index < 0 || index >= this.size) return null
-    let count = 0
-    let node = this.head
+  getNode(index: number): LLNode {
+    if (index < 0 || index >= this.size) return null;
+    let count = 0;
+    let node = this.head;
     while (count < index) {
-      node = node?.next
-      count++
+      node = node?.next;
+      count++;
     }
-    return node
-    }
+    return node;
+  }
 
-    get(index: number): number {
-        const node: LLNode | null = this.getNode(index);
-        if(!node) return -1;
-        return node.val;
-    }
+  get(index: number): number {
+    const node: LLNode | null = this.getNode(index);
+    if (!node) return -1;
+    return node.val;
+  }
 
-    addAtHead(val: number): void {
-        this.head = new LLNode(val, this.head);
-        this.size++;
-    }
+  addAtHead(val: number): void {
+    this.head = new LLNode(val, this.head);
+    this.size++;
+  }
 
-    addAtTail(val: number): void {
-        if (this.size === 0) {
-            this.addAtHead(val);
-            return;
-        }
-
-        const node: LLNode | null = this.getNode(this.size - 1);
-        if (!node) return;
-
-        node.next = new LLNode(val, null);
-        this.size++;
+  addAtTail(val: number): void {
+    if (this.size === 0) {
+      this.addAtHead(val);
+      return;
     }
 
-    addAtIndex(index: number, val: number): void {
-        if (index < 0 || index > this.size) return;
+    const node: LLNode | null = this.getNode(this.size - 1);
+    if (!node) return;
 
-        if (index === 0) {
-            this.addAtHead(val);
-            return;
-        }
+    node.next = new LLNode(val, null);
+    this.size++;
+  }
 
-        if (index === this.size) {
-            this.addAtTail(val);
-            return;
-        }
+  addAtIndex(index: number, val: number): void {
+    if (index < 0 || index > this.size) return;
 
-        const node: LLNode | null = this.getNode(index - 1);
-        if (!node) return;
-        node.next = new LLNode(val, node.next);
-        this.size++;
+    if (index === 0) {
+      this.addAtHead(val);
+      return;
     }
 
-    deleteAtIndex(index: number): void {
-        if (index < 0 || index >= this.size) return;
-        if (this.size === 0) return;
-
-        if (index === 0) {
-            this.head = this.head.next;
-            this.size--;
-            return;
-        }
-
-        const node: LLNode | null = this.getNode(index - 1);
-        if (!node) return;
-
-        node.next = node.next.next || null;
-        this.size--;
+    if (index === this.size) {
+      this.addAtTail(val);
+      return;
     }
+
+    const node: LLNode | null = this.getNode(index - 1);
+    if (!node) return;
+    node.next = new LLNode(val, node.next);
+    this.size++;
+  }
+
+  deleteAtIndex(index: number): void {
+    if (index < 0 || index >= this.size) return;
+    if (this.size === 0) return;
+
+    if (index === 0) {
+      this.head = this.head.next;
+      this.size--;
+      return;
+    }
+
+    const node: LLNode | null = this.getNode(index - 1);
+    if (!node) return;
+
+    node.next = node.next.next || null;
+    this.size--;
+  }
 }
 
 /**
