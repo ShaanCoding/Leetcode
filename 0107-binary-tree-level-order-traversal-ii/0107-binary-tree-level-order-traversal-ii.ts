e12d4d19c8f79ce8b5c8f15b8eddaa8518b2c1f0
Shaan Khan <shaankhan101@gmail.com>
2023-08-07 02:44:09 +1000

    Time: 71 ms (45.16%), Space: 44.8 MB (74.19%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+//     Just BFS but reversed?
+    if(!root) return [];
+    
+    let returnArray: number[][] = [];
+    let queue: TreeNode[] = [root];
+    
+    while(queue.length !== 0) {
+        let length: number = queue.length;
+        let level: number[] = [];
+        
+        for(let i = 0; i < length; i++) {
+            let node: TreeNode | null = queue[0];
+            queue.shift();
+            if(!node) break;
+            
+            level.push(node.val);
+            if(node?.left) queue.push(node.left);
+            if(node?.right) queue.push(node.right);
+        }
+        returnArray.push(level);
+    }
+    
+    return returnArray.reverse();
+};
