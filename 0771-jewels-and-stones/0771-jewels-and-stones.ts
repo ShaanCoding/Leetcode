ad929218a18c96d6891c5a0122f68fd793b098f7
Shaan Khan <shaankhan101@gmail.com>
2023-08-06 21:02:24 +1000

    Time: 50 ms (97.97%), Space: 44.4 MB (60.68%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.ts
+++ 0771-jewels-and-stones/0771-jewels-and-stones.ts
@@ -1,10 +1,4 @@
 function numJewelsInStones(jewels: string, stones: string): number {
-//     Jewels and stones representing stones, a stone is a type of stone you have
-    // a is considered a different type
-    // want to know how many stones you have are also jewels
-    // Hashmap of both and then compare // union?
-    
-    
     let jSet: Set<string> = new Set<string>(jewels.split(''));
 
     let numberOfStones = 0;
