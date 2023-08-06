358c7bb1a238449c9e3a5d1575911e99fe68104a
Shaan Khan <shaankhan101@gmail.com>
2023-08-06 21:00:32 +1000

    Time: 54 ms (92.54%), Space: 44.9 MB (26.10%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.ts
+++ 0771-jewels-and-stones/0771-jewels-and-stones.ts
@@ -4,21 +4,18 @@ function numJewelsInStones(jewels: string, stones: string): number {
     // want to know how many stones you have are also jewels
     // Hashmap of both and then compare // union?
     
+    
     let jMap: Map<string, number> = new Map<string, number>();
     let sMap: Map<string, number> = new Map<string, number>();
     
     for(let s of jewels) {
         jMap.set(s, (jMap.get(s) || 0) + 1);
     }
-    
-    for(let s of stones) {
-        sMap.set(s, (sMap.get(s) || 0) + 1);
-    }
-    
+
     let numberOfStones = 0;
     
-    for(let [key, value] of sMap) {
-        if(jMap.has(key)) numberOfStones += value;
+    for(let s of stones) {
+        if(jMap.has(s)) numberOfStones++;
     }
     
     return numberOfStones;
