2cac24bc853899ca6b7406f2c5647eac4c44acf2
Shaan Khan <shaankhan101@gmail.com>
2023-08-06 22:00:32 +1000

    Time: 58 ms (88.60%), Space: 46.3 MB (32.79%) - LeetHub

--- 0704-binary-search/0704-binary-search.ts
+++ 0704-binary-search/0704-binary-search.ts
@@ -1,22 +1,21 @@
 function search(nums: number[], target: number): number {
+    // Return index of item otherwise -1
+    
     let low: number = 0;
     let high: number = nums.length - 1;
-
-    while(low <= high) {
-        let mid: number = Math.floor((low + high) / 2);
+    
+    while(high >= low) {
+        let mid: number = low + Math.floor((high - low) / 2);
         
         if(nums[mid] == target) return mid;
-
-        // Left side
+        if(mid + 1 < nums.length && nums[mid + 1] == target) return mid + 1;
+        
         if(nums[mid] > target) {
-            high = mid - 1;
-        }
-
-        // Right side
-        if(nums[mid] < target) {
+           high = mid - 1; 
+        } else {
             low = mid + 1;
         }
     }
-
+    
     return -1;
 };
