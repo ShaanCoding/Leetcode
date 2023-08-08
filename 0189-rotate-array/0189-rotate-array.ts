faed35f34b08809c5cdaebc0695910ac24c492cf
Shaan Khan <shaankhan101@gmail.com>
2023-08-08 21:42:34 +1000

    Time: 75 ms (99.18%), Space: 52.4 MB (56.10%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.ts
@@ -0,0 +1,19 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+    // +3 || if <= nums.length then we - nums.length to it
+    
+    let rotatedArray: number[] = [];
+
+    for(let i = 0; i < nums.length; i++) {
+        let index = i + k;
+        while(index >= nums.length) index -= nums.length; 
+        rotatedArray[index] = nums[i];
+    }
+
+    for(let i = 0; i < nums.length; i++) {
+        nums[i] = rotatedArray[i];
+    }
+
+};
