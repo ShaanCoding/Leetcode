f284ca266d82ee13b0d6d31ee6c1b1e3255fe353
Shaan Khan <shaankhan101@gmail.com>
2023-09-04 17:11:52 +1000

    Time: 52 ms (81.34%), Space: 44.2 MB (52.51%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
@@ -1,29 +1,22 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
+  let width = matrix[matrix.length - 1].length;
 
-//     Find if element exists, needs to be in log time
-    
-    let width = matrix[matrix.length - 1].length;
-    
-    let left: number = 0, right = matrix.length * width - 1;
-    
-    while(left <= right) {
-        let midPoint = Math.floor((right + left) / 2);
-        
-        let x = midPoint % width;
-        // How many are divisible by
-        let y = Math.floor((midPoint) / width);
-        
-        let element = matrix[y][x];
-        
-        if(element === target) return true;
-        else if (element > target) {
-            // Take "left"
-            right = midPoint - 1;
-        } else {
-            left = midPoint + 1;
-        }
-        
+  let left: number = 0,
+    right = matrix.length * width - 1;
+
+  while (left <= right) {
+    let midPoint = Math.floor((right + left) / 2);
+
+    let x = midPoint % width;
+    let y = Math.floor(midPoint / width);
+
+    if (matrix[y][x] === target) return true;
+    else if (matrix[y][x] > target) {
+      right = midPoint - 1;
+    } else {
+      left = midPoint + 1;
     }
-    
-    return false;
-};
+  }
+
+  return false;
+}
