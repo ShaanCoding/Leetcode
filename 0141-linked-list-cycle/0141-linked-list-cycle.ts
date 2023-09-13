355c513096a4088c5663917c35d41aa354baf8d7
Shaan Khan <shaankhan101@gmail.com>
2023-09-13 22:38:52 +1000

    Time: 74 ms (33.78%), Space: 47.1 MB (7.42%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.ts
+++ 0141-linked-list-cycle/0141-linked-list-cycle.ts
@@ -11,18 +11,18 @@
  */
 
 function hasCycle(head: ListNode | null): boolean {
-    // Set see if it's visited, if we reach null then it's fine
-
-    let navigatorHead: ListNode | null = head;
-    let visitedSet: Set<ListNode | null> = new Set();
-
-    while(navigatorHead != null) {
-        if(visitedSet.has(navigatorHead)) return true;
-        else {
-            visitedSet.add(navigatorHead);
-            navigatorHead = navigatorHead?.next;
-        }
+    if(!head) return false;
+    
+    // Check if cycle, means check if the thin exists already (need to go by memory addresses)
+    let cycleSet: Set<ListNode> = new Set<ListNode>();
+    
+    let traversalNode = head;
+    
+    while(traversalNode !== null) {
+        if(cycleSet.has(traversalNode)) return true;
+        cycleSet.add(traversalNode);
+        traversalNode = traversalNode?.next;
     }
-
+    
     return false;
 };
