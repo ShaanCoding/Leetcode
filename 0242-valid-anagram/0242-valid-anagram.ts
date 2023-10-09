94115207aba826370fb3072017ac8af945707921
Shaan Khan <shaankhan101@gmail.com>
2023-10-09 21:25:09 +1100

    Time: 68 ms (80.91%), Space: 44.6 MB (83.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,39 +1,20 @@
 function isAnagram(s: string, t: string): boolean {
-    // s = 'anagram', t = 'nagaram'
-    // return true if anagram, return false if not
-
-    // Map method -> We have key values go through both arrays and compare if maps are same length, 3N
-
     if(s.length !== t.length) return false;
-
-    let sMap = new Map();
-
-    let tMap = new Map();
-
-    let sSplit = s.split('');
-    let tSplit = t.split('');
-
-    for(let i = 0; i < sSplit.length; i++) {
-        if(sMap.has(sSplit[i])) {
-            sMap.set(sSplit[i], sMap.get(sSplit[i]) + 1);
-        } else {
-            sMap.set(sSplit[i], 1);
-        }
+    
+    let sFreqMap: Map<string, number> = new Map();
+    let tFreqMap: Map<string, number> = new Map();
+    
+    for(let character of s) {
+        sFreqMap.set(character, sFreqMap.has(character) ? sFreqMap.get(character) + 1 : 1);
     }
-
-    for(let i = 0 ; i < tSplit.length; i++) {
-        if(tMap.has(tSplit[i])) {
-            tMap.set(tSplit[i], tMap.get(tSplit[i]) + 1);
-        } else {
-            tMap.set(tSplit[i], 1);
-        }
+    
+    for(let character of t) {
+        tFreqMap.set(character, tFreqMap.has(character) ? tFreqMap.get(character) + 1 : 1);
     }
-
-    if(sMap.size !== tMap.size) return false;
-
-    for (let [key, value] of sMap.entries()) {
-        if(value !== tMap.get(key)) return false;
+    
+    for(let [key, value] of sFreqMap) {
+        if(tFreqMap.get(key) !== value) return false;
     }
-
+    
     return true;
 };
