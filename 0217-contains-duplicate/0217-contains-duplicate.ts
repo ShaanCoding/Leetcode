e1856badf5e3612651c645abe7fd0e034a736bc2
Shaan Khan <shaankhan101@gmail.com>
2023-10-09 15:39:00 +1100

    Time: 87 ms (44.58%), Space: 55 MB (50.12%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -1,6 +1,4 @@
 function containsDuplicate(nums: number[]): boolean {
-//     Return true if value appears at least twice, return false if distinct
-    
     let arrayLength: number = nums.length;
     let setLength: number = new Set<number>(nums).size;
     
